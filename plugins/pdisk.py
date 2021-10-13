5885e4390b083df50fa259c971bc16dea3307005
Akash Sharma <89353868+AkashFlix@users.noreply.github.com>
2021-10-13 16:04:16 +0530

    Update pdisk.py

--- plugins/pdisk.py
+++ plugins/pdisk.py
@@ -50,7 +50,7 @@ async def pdisk(bot, message):
                 photo="https://telegra.ph/file/7389a320199583a2ac493.jpg",
                 caption="\n**URL:** `"+pdisk+"`\n\n**The PDisk Link Is Below, The Provided Link Will Be Uploaded in few minutes...so be patient\n━━━━━━━━━━━━━━━━━━━━\n⦿ Made With♥️BY @OO7ROBot\n━━━━━━━━━━━━━━━━━━━━**",
                 reply_markup=InlineKeyboardMarkup([
-                    [ InlineKeyboardButton(text="🔗 PDisk Link", url=f"{pdisk}"), InlineKeyboardButton(text="Channel", url="https://telegram.me/MyTestBotZ")]
+                    [ InlineKeyboardButton(text="🔗 PDisk Link", url=f"{pdisk}"), InlineKeyboardButton(text="Channel", url="https://telegram.me/MoviesHubFlix")]
                 ])
             )
 
